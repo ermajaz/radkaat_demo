bc4cd4f34f35a36657fa9a7fad0313ead2c4ae5d
Majazul Haque <142217528+ermajaz@users.noreply.github.com>
2025-10-07 17:51:21 +0530

    update bugs

--- src/app/bikes/saola/page.tsx
+++ src/app/bikes/saola/page.tsx
@@ -5,11 +5,11 @@ import BikeHero from "@/components/bikes/hero/BikeHero";
 import BikeStrip from "@/components/bikes/strip/BikeStrip";
 import FeelTheFuture from "@/components/bikes/goat/FeelTheFuture";
 import FeaturesFirstSection from "@/components/bikes/features/FeaturesFirstSection";
-import FeatureBlock from "@/components/bikes/features/FeatureBlock";
-import GeometryTable from "@/components/bikes/features/GeometryTable";
 import ParallaxWrapper from "@/components/common/ParallaxWrapper";
 import TestRideSection from "@/components/landing/testRide/TestRideSection";
-import Highlights from "@/components/bikes/features/Highlights";
+import FeatureShowcase from "@/components/bikes/features/FeatureShowcase";
+import SpecsCompareSection from "@/components/bikes/geometry/SpecsCompareSection";
+import SpecsExperienceSection from "@/components/bikes/specsToExperience/SpecsExperienceSection";
 
 export default function SaolaPage() {
   const router = useRouter();
@@ -43,65 +43,64 @@ export default function SaolaPage() {
         />
         <FeelTheFuture />
         <FeaturesFirstSection />
-        <FeatureBlock
-          index={1}
-          label="Lorem Ipsum"
-          title="Lightweight Alloy Frame"
-          points={[
-            "Material: 6061 Aluminum Alloy",
-            "Weight: 11.8kg(medium size frame",
-            "Benefit: Easy handling and better speed on roads",
-          ]}
-          image="/images/bikes/frameset.png"
-        />
-        <FeatureBlock
-          index={2}
-          label="Lorem Ipsum"
-          title="21-Speed Shimano Gear System"
-          points={[
-            "Type: Shimano Tourney TZ",
-            "Gears: 3 front * 7 rear = 21-speed",
-            "Benefit: Smooth gear shifting for upchill climbs & flat roads",
-          ]}
-          image="/images/bikes/crank.png"
-        />
-        <FeatureBlock
-          index={3}
-          label="Lorem Ipsum"
-          title="Responsiveness Stability"
-          points={["Steel: 67.3", "Alloy Non Boost: 67", "Alloy Boost: 67"]}
-          image="/images/bikes/cycle-f.png"
-        />
-        <FeatureBlock
-          index={4}
-          label="Lorem Ipsum"
-          title="Range of User Height How Tall A Bike Feels"
-          points={[
-            "Steel: 525mm",
-            "Alloy Non Boost: 625mm",
-            "Alloy Boost: 625mm",
-          ]}
-          image="/images/bikes/cycle-f.png"
-        />
-        <FeatureBlock
-          index={5}
-          label="Lorem Ipsum"
-          title="Bike Control How Long A Bike Feels"
-          points={[
-            "Steel: 380mm",
-            "Alloy Non Boost: 440mm",
-            "Alloy Boost: 440mm",
-          ]}
-          image="/images/bikes/cycle-f.png"
-        />
-        <GeometryTable />
-        <Highlights
-          images={[
-            "/images/bikes/bike-highlight-1.jpg",
-            "/images/bikes/bike-highlight-2.jpg",
-            "/images/bikes/bike-highlight-3.jpg",
+        <FeatureShowcase
+          features={[
+            {
+              index: 1,
+              label: "Lorem Ipsum",
+              title: "Lightweight Alloy Frame",
+              points: [
+                "Material: 6061 Aluminum Alloy",
+                "Weight: 11.8kg (medium size frame)",
+                "Benefit: Easy handling and better speed on roads",
+              ],
+              image: "/images/bikes/frameset.png",
+            },
+            {
+              index: 2,
+              label: "Lorem Ipsum",
+              title: "21-Speed Shimano Gear System",
+              points: [
+                "Type: Shimano Tourney TZ",
+                "Gears: 3 front * 7 rear = 21-speed",
+                "Benefit: Smooth gear shifting for uphill climbs & flat roads",
+              ],
+              image: "/images/bikes/crank.png",
+            },
+            {
+              index: 3,
+              label: "Lorem Ipsum",
+              title: "Responsiveness Stability",
+              points: ["Steel: 67.3", "Alloy Non Boost: 67", "Alloy Boost: 67"],
+              image: "/images/bikes/cycle-f.png",
+            },
+            {
+              index: 4,
+              label: "Lorem Ipsum",
+              title: "Range of User Height How Tall A Bike Feels",
+              points: [
+                "Steel: 525mm",
+                "Alloy Non Boost: 625mm",
+                "Alloy Boost: 625mm",
+              ],
+              image: "/images/bikes/cycle-f.png",
+            },
+            {
+              index: 5,
+              label: "Lorem Ipsum",
+              title: "Bike Control How Long A Bike Feels",
+              points: [
+                "Steel: 380mm",
+                "Alloy Non Boost: 440mm",
+                "Alloy Boost: 440mm",
+              ],
+              image: "/images/bikes/cycle-f.png",
+            },
           ]}
         />
+
+        <SpecsCompareSection />
+        {/* <SpecsExperienceSection/> */}
         <div className="py-5">
           <TestRideSection />
         </div>
